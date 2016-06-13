4ffc0d7ebb6da7c2386980b8e6c5b6e9cb000bbf
Cyril PERROT <perrotcy@gmail.com>
2016-06-13 15:50:55 +0200

    fix typo

--- Datatable/Data/DatatableQuery.php
+++ Datatable/Data/DatatableQuery.php
@@ -855,7 +855,7 @@ class DatatableQuery
         $outputHeader = array(
             'draw' => (int) $this->requestParams['draw'],
             'recordsTotal' => (int) $this->getCountAllResults($this->rootEntityIdentifier),
-            'recordsFiltered' => (int) count($fresults);
+            'recordsFiltered' => (int) count($fresults),
         );
 
         $json = $this->serializer->serialize(array_merge($outputHeader, $output), 'json');
